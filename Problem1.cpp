118da3d23c72d56f2cdd0c17b466c8927dc61b92
sahildps <sahildshetty@gmail.com>
2025-01-05 10:03:24 -0700

    Competitive-Coding-1

--- Problem1.cpp
+++ Problem1.cpp
@@ -1,1 +1,58 @@
 
+// i missing element in between the array
+// Time complexity : O(log n)
+// Space complexity : O(1)
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int MissingElement(vector<int> nums)
+{
+    if (nums.empty())
+    {
+        return -1;
+    }
+
+    int low = 0, high = nums.size() - 1;
+
+    while (low <= high)
+    {
+
+        if (high - low == 1 && nums[high] - nums[low] != 1)
+        {
+            return nums[low] + 1;
+        }
+
+        int mid = low + (high - low) / 2;
+
+        if (nums[low] - low != nums[mid] - mid)
+        {
+            high = mid;
+        }
+        else
+        {
+            low = mid;
+        }
+    }
+
+    return -1;
+}
+
+int main()
+{
+    vector<int> data = {1, 2, 3, 5, 6, 7, 8};
+
+    int result = MissingElement(data);
+    if (result != -1)
+    {
+        cout << "Missing element is " << result << endl;
+    }
+    else
+    {
+        cout << " There are no missing elements" << endl;
+    }
+
+    return 0;
+}
